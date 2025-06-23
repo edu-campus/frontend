b9d22a5753d665e703700e388f94053f419a0259
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-06-23 12:39:33 +0200

    ➕ add 'shadcn-vue' as design library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
+  css: ['~/assets/tailwind.css'],
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon']
+  modules: ['@nuxt/eslint', '@nuxt/icon', 'shadcn-nuxt'],
+  vite: {
+    plugins: [
+      tailwindcss()
+    ],
+  },
+  shadcn: {
+    componentDir: './components/ui'
+  }
 })
