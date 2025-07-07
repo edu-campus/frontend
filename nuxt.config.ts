6cfd9f63aa43363586302f94d21714d6f07076bc
Sami Arda Ünsay <192653549+samiuens@users.noreply.github.com>
2025-07-07 18:12:26 +0200

    add global css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  css: ['~/assets/tailwind.css'],
+  css: ['~/assets/global.css', '~/assets/tailwind.css'],
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/icon', 'shadcn-nuxt'],
   vite: {
--- pages/onboarding/index.vue
+++ pages/onboarding/index.vue
@@ -1,3 +1,7 @@
 <template>
-    
+    <div class="bg-gray-200 w-full h-full flex">
+        <div class="bg-white max-w-3xl rounded-md p-4 m-auto">
+            <p>test</p>
+        </div>
+    </div>
 </template>
